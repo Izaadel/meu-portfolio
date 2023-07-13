55117dc9564ea8425f02e57f2f0f4b14b98f9c9b
Isabela Del Vecchio Bueno Goes <123563738+Izaadel@users.noreply.github.com>
2023-07-13 19:23:16 -0300

    Add files via upload

---
+++ sript.js
@@ -0,0 +1,8 @@
+//continuação do eu sou uma...//
+var typed = new Typed(".text", {
+    strings:[ "Desenvolvedora Web", "Designer","Front-End", "PJ"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay: 1000,
+    loop:true
+});
